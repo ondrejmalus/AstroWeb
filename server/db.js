3e0f8f0236b9dd8df5f6e09ae7b32047924bc228
ondrejmalus <ondrama06@gmail.com>
2026-03-29 17:45:16 +0200

    oprava db

--- server/db.js
+++ server/db.js
@@ -1,10 +1,8 @@
 import mysql from 'mysql2/promise';
 
 export const db = await mysql.createPool({
-  host: 'store6.rosti.cz',
-  user: 'ondrama0_3355',
-  password: 'Ja2072006.',
-  database: 'ondrama0_3355',
-  waitForConnections: true,
-  connectionLimit: 10
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'astroweb'
 });
